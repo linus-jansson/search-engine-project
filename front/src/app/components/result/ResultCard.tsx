eba9af70bfe02e1ec856f3feae57557ee91c130b
Linus J <linus.jansson2000@gmail.com>
2023-01-22 22:28:11 +0100

    modify colors of page

--- front/src/app/components/result/ResultCard.tsx
+++ front/src/app/components/result/ResultCard.tsx
@@ -13,8 +13,8 @@ export default function SearchResultCard(props: any) {
                     <Link href={props.url} className="text-2xl">{props.title}</Link>
                 </div>
                 <div id="desc">
-                    <span className='mr-2 text-neutral-400'>{formatDate(props.date)}</span>
-                    <span className='text-neutral-300'>{props.summary}</span>
+                    <span className='mr-2 text-zinc-400'>{formatDate(props.date)}</span>
+                    <span className='text-zinc-300'>{props.summary}</span>
                 </div>
             </div>
         </>
