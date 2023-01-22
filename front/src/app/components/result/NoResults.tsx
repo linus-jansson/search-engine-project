97a6a1675ea1309a27e521bfb80f0cfb1fc16fcd
Linus J <linus.jansson2000@gmail.com>
2023-01-22 23:30:18 +0100

    Fix build errors

--- front/src/app/components/result/NoResults.tsx
+++ front/src/app/components/result/NoResults.tsx
@@ -4,7 +4,7 @@ export default function NoResults({ query }: { query: string }) {
 
     return (
         <div className='flex flex-col w-96'>
-            <h1 className="text-3xl font-bold mb-3">No results found for "{query}"</h1>
+            <h1 className="text-3xl font-bold mb-3">No results found for &quot;{query}&quot;</h1>
             <p>Would you like to search using <span className='font-bold'>DuckDuckGo</span> instead?</p>
             <div className='mt-2'>
                 <Link href={`https://duckduckgo.com/?q=${query}`} className="font-bold border p-1 rounded-lg border-white text-lg duration-100 hover:bg-zinc-500 hover:text-zinc-900">Search</Link>
--- front/src/app/search/page.tsx
+++ front/src/app/search/page.tsx
@@ -1,30 +1,22 @@
+import Link from 'next/link';
 
-import NoResults from '../components/result/NoResults';
-import ResultCard from '../components/result/ResultCard';
 import SearchField from '../components/search/SearchField';
+import SearchResults from '../components/result/SearchResults';
 
-export const revalidate = 300; // revalidate this page every 5 minutes
+export default function SearchPage({ searchParams }: { [key: string]: string | string[] | undefined }) {
+    if (searchParams === undefined || !searchParams.hasOwnProperty("q")) return <div>Search params are undefined</div>;
+    let searchQuery = searchParams["q" as any];
 
-async function GetSearchResults(query: string) { // leave it as any for now
-    const apiResponse = await fetch(`http://localhost:3000/api/search?q=${query}`, { cache: 'default' })
-    return apiResponse.json();
-}
-
-export default async function SearchPage({ searchParams }: { [key: string]: string | string[] | undefined }) {
-    let searchQuery = searchParams.q;
-    const searchResults = await GetSearchResults(searchQuery);
     return (
         <div>
             <div id="top-bar" className='w-screen shadow-3xl bg-neutral-900 flex flex-col p-4 items-center md:flex-row'>
-                <h1 className="text-3xl font-bold mr-6">Search stuff</h1>
+                <Link href='/' className="text-3xl font-bold mr-6">Search stuff</Link>
                 <SearchField currentQuery={searchQuery} />
             </div>
-            <div id="search-results" className='ml-4 mt-4'>
-                {/* TODO: In future if no results show no results found */}
-                {/* {searchResults.length === 0 && <NoResults query={searchQuery} />} */}
-                <NoResults query={searchQuery} />
-                {searchResults && searchResults.map((data: unknown, idx: number) => <ResultCard key={idx} {...data} />)}
-            </div>
+
+            {/* https://github.com/vercel/next.js/issues/42292 */}
+            {/* @ts-expect-error Server Component */}
+            <SearchResults query={searchQuery} />
         </div>
     )
 }
