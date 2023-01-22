f19d5da367a426cda31f975dc173c45926633694
Linus J <linus.jansson2000@gmail.com>
2023-01-22 21:20:09 +0100

    Todo searchParams on searchpage
    Being worked on by the nextjs team: https://github.com/vercel/next.js/pull/43305

---
+++ front/src/app/search/head.tsx
@@ -0,0 +1,9 @@
+export default function Head({ searchParams }: { [key: string]: string | string[] | undefined }) {
+    // TODO: Add search query to title
+    // https://github.com/vercel/next.js/pull/43305
+    return (
+        <>
+            <title>This is being worked on by nextjs team</title>
+        </>
+    );
+}
