29582bf73c5ef652a5b1f41bdeba1e23a8523bd7
Linus J <linus.jansson2000@gmail.com>
2023-01-23 14:16:53 +0100

    Update db_schema.sql

--- back/db_schema.sql
+++ back/db_schema.sql
@@ -1,4 +1,7 @@
 CREATE TABLE page (
   url TEXT NOT NULL PRIMARY KEY,
+  title TEXT NOT NULL,
+  summary TEXT NOT NULL,
+  full_text TEXT NOT NULL, -- Store the full text of the page to easier get what the client is looking for
   fetch_time TIMESTAMP NOT NULL
 );
