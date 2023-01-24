d25bee2a6589d81247d9d193b2c0ef4f72c8fc27
Linus J <linus.jansson2000@gmail.com>
2023-01-24 22:28:30 +0100

    Remove of autoimport

--- front/src/app/components/search/SearchField.tsx
+++ front/src/app/components/search/SearchField.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useRouter } from "next/navigation";
-import Router from "next/router";
 import { useRef } from "react";
 
 export default function SearchField({ currentQuery }: { currentQuery: string | undefined }) {
