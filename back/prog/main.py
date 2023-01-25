57e272159fc38b14001aaa2a298d6b975937bb14
Linus J <linus.jansson2000@gmail.com>
2023-01-25 17:42:15 +0100

    Add Parse query and return scraped data rest API

--- back/prog/main.py
+++ back/prog/main.py
@@ -1,6 +1,6 @@
 
 import sys
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -21,9 +21,23 @@ def getSearchResults():
 def saveURL():
     pass
 
-@app.route('/api/testScraperURL')
-def testPageScraper():
-    pass
+@app.route('/api/getURLData')
+def testPageScraper(url=None):
+    import scraper
+    import _parser as parser
+    # get url from body from request
+    url = request.args.get('q')
+    if url is None:
+        return jsonify({'error': 'No URL provided'}), 400
+    print(url)
+
+    url_list = url.split(',')
+    output_data = []
+    for url, data in scraper.scrapeURLS(url_list, 60, 20):
+        new_page = parser.Page(url, data)
+        output_data.append(new_page.pageObject)
+
+    return jsonify(output_data), 200
 
 if __name__ == '__main__':
     shouldRunDebug = "debug" in sys.argv
