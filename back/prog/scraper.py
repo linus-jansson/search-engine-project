bd3a3396b29ce008eb47ae83c837fe0d3a0bb2e9
Linus J <linus.jansson2000@gmail.com>
2023-01-25 17:05:23 +0100

    Made scrapeUrls more generic

--- back/prog/scraper.py
+++ back/prog/scraper.py
@@ -23,19 +23,19 @@ def load_url(url, timeout):
     with urllib.request.urlopen(request, timeout=timeout) as conn:
         return conn.read()
 
-
-def main():
+def scrapeURLS(listOfUrls: list, timeout=60, maxWorkers=10) :
     URLerrors = 0
 
     # We can use a with statement to ensure threads are cleaned up promptly
-    with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=maxWorkers) as executor:
         # Start the load operations and mark each future with its URL
-        future_to_url = {executor.submit(load_url, url, 60): url for url in URLS}
+        future_to_url = {executor.submit(load_url, url, timeout): url for url in listOfUrls}
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
             try:
                 data = future.result()
                 parser.testParseWebpageContent(url, data) ## saves data to file
+                yield url, future.result()
             except Exception as exc:
                 print('%r generated an exception: %s' % (url, exc))
                 URLerrors += 1
@@ -44,6 +44,17 @@ def main():
 
         print(f"URLs tested: {len(URLS)} URL errors: {str(URLerrors)}/{len(URLS)}")
 
+
+
+def main():
+    pages = []
+    for url, data in scrapeURLS(URLS, 60, 20):
+        page = parser.Page(url, data)
+        print("adding ", page.pageTitle)
+        pages.append(page)
+    
+    print(f"Pages scraped successfully: {len(pages)}")
+        
 
 if __name__ == "__main__":
     main()
