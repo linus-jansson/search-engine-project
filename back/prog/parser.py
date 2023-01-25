d7a1fc309f47444f92dc9f5d88f1b7f4deeae698
Linus J <linus.jansson2000@gmail.com>
2023-01-25 15:29:40 +0100

    Add BS4 to parse data from URLLib

--- back/prog/parser.py
+++ back/prog/parser.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+from bs4 import BeautifulSoup as bs4
+import time
+
+
 def testParseWebpageContent(url, data):
     # Check if website folder exists
     if not Path("websites").exists():
@@ -7,7 +11,27 @@ def testParseWebpageContent(url, data):
     # get number of files in websites folder
     num_files = len([f for f in Path("websites").iterdir() if f.is_file()])
 
+    soup = bs4(data, 'html.parser')
+    pageTitle = soup.title.string
+    pageText = soup.get_text().replace("\n","").strip()
+    pageUrl = url
+    currentDateEpoch = int(time.time())
+
+    outString = f"""\n
+    {"#"*50}\n
+    URL: {pageUrl}\n
+    Title: {pageTitle}\n
+    PageContent: {pageText}\n
+    Date: {currentDateEpoch}\n
+    {"#"*50}\n\n
+    """
     # Saves data to file
-    with open(f"websites/{num_files}.html", "wb") as f:
-        f.write(data)
+    try:
+        with open(f"websites/pages.html", "a+", encoding="utf-8") as f:
+            f.write(outString)
+    except Exception as exc:
+        print("Error saving file")
+        print('%r generated an exception: %s' % (url, exc))
+        
+
     
