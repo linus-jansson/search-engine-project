ac96dfae65dae36701f83926be7852fb3b78a877
Linus J <linus.jansson2000@gmail.com>
2023-01-25 15:11:50 +0100

    Simple Flask webserver

---
+++ back/prog/main.py
@@ -0,0 +1,26 @@
+
+import sys
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def API_test():
+    output = {
+        'test': 3
+    }
+
+    return jsonify(output)
+
+
+@app.route('/api/getURLMatches')
+def getSearchResults():
+    pass
+
+@app.route('/api/saveURL')
+def saveURL():
+    pass
+
+if __name__ == '__main__':
+    shouldRunDebug = "debug" in sys.argv
+    app.run(debug=shouldRunDebug)
