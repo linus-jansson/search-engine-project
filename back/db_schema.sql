2871a10ba78a75162c7af4e35568ac747aabe66a
Linus J <linus.jansson2000@gmail.com>
2023-01-25 15:12:01 +0100

    Added notParsed table

--- back/db_schema.sql
+++ back/db_schema.sql
@@ -1,7 +1,12 @@
-CREATE TABLE page (
+CREATE TABLE IF NOT EXISTS parsedPages (
   url TEXT NOT NULL PRIMARY KEY,
   title TEXT NOT NULL,
   summary TEXT NOT NULL,
   full_text TEXT NOT NULL, -- Store the full text of the page to easier get what the client is looking for
   fetch_time TIMESTAMP NOT NULL
 );
+
+
+CREATE TABLE IF NOT EXISTS notParsedPages (
+    url TEXT NOT NULL PRIMARY KEY,
+)
