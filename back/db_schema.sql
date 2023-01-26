32463bd63437b4ec9d370df85c36db8e807c416e
Peter J <21022916+peter-jansson@users.noreply.github.com>
2023-01-26 18:32:35 +0100

    Update db_schema.sql

--- back/db_schema.sql
+++ back/db_schema.sql
@@ -1,12 +1,24 @@
+CREATE TABLE IF NOT EXISTS words (
+  id INTEGER NOT NULL PRIMARY KEY,
+  word TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS parsedPages (
-  url TEXT NOT NULL PRIMARY KEY,
+  id INTEGER NOT NULL PRIMARY KEY,
+  url TEXT NOT NULL,
   title TEXT NOT NULL,
   summary TEXT NOT NULL,
   full_text TEXT NOT NULL, -- Store the full text of the page to easier get what the client is looking for
   fetch_time TIMESTAMP NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS notParsedPages (
-    url TEXT NOT NULL PRIMARY KEY,
-)
+  id INTEGER NOT NULL PRIMARY KEY,
+  url TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS words_urls (
+  word_id INTEGER NOT NULL CONSTRAINT words_urls_word_id_fk REFERENCES words(id),
+  url_id INTEGER NOT NULL CONSTRAINT words_urls_url_id_fk REFERENCES parsedPages(id),
+  CONSTRAINT words_urls_word_id_uq UNIQUE (word_id, url_id)
+);
