096d9ce4e016882f40228c612b8d9bc687ce49da
Linus J <linus.jansson2000@gmail.com>
2023-01-27 11:13:24 +0100

    Add a method to execute a list of queries

--- back/src/database/database.py
+++ back/src/database/database.py
@@ -14,40 +14,60 @@ class Database():
             print('SQLite version:', data)
 
         self.initTables()
-        # ...
 
     def initTables(self):
-        # ...
         schemaPath = Path(__file__).parent.absolute()/Path('db_schema.sql')
 
         # If database empty initialize tables
+        queries = []
+        with open(schemaPath, 'r') as f:
+            currentQuery = ""
+            for lines in f.readlines():
+                currentQuery += lines
+                if lines.endswith(";\n"):
+                    queries.append(currentQuery)
+                    currentQuery = ""
+
+        self.executeQueries(queries)
+
+    def executeQueries(self, queries: list[list[str, str]]):
+        """
+        Executes a list of queries
+        :param queries: List of queries to execute [[query, parameters], [query, parameters], ...]
+        """
         self.connection.isolation_level = None
         cursor = self.connection.cursor()
         cursor.execute("BEGIN")
         try:
-            # Parse db_schema.sql
-            with open(schemaPath, 'r') as f:
-                currentQuery = ""
-                for lines in f.readlines():
-                    currentQuery += lines
-                    if lines.endswith(";\n"):
-                        cursor.execute(currentQuery)
-                        currentQuery = ""
+            # Todo: if a tuple of query, parameter is passed execute it and commit
+
+            for querySet in queries:
+                query = querySet[0]
+                if len(querySet) > 1:
+                    parameters = querySet[1]
+                    cursor.execute(query, parameters)
+                else:
+                    cursor.execute(query)
 
             cursor.execute("COMMIT")
         except Exception as exc:
             self.connection.execute("ROLLBACK")
             raise Exception(
-                "Error: Unable to parse db_schema.sql (rolling back): %s" % exc.message)
+                "Error: Unable to execute queries (rolling back): %s" % exc.message)
 
     def addURL(self, url):
-        # ...
-        self.connection.execute(
-            "INSERT INTO urls (url, date) VALUES (?, ?)", (url, self.currentDateEpoch))
-        self.connection.commit()
-        # ...
+        pass
 
     def addPage(self, url, page):
+        """Example code"""
+        query = "INSERT INTO pages (url, title, content, ogTitle, ogType, ogImage, ogUrl, ogDescription, ogLocale, praseDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+        parameters = (url, page.title, page.words, page.ogTitle, page.ogType,
+                      page.ogImage, page.ogUrl, page.ogDescription, page.ogLocale, page.praseDate)
+        self.executeQueries([[query, parameters]])
+        """"""
+
+    def addWords(self, words: list):
+        pass
 
 
 if __name__ == "__main__":
